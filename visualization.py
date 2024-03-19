38f551a331390f7cf1a35568e2418e25e8522d9c
Kieran Mansk <kmanske343@lsr7.net>
2024-03-19 07:59:01 -0500

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-file = pd.read_excel('SAGDP2N_ALL_AREAS_1997_2022_cleaned.xlsx') 
 
-#create a list of lists to store the data for every idustry in every state
-state_data = []
-for state in file['GeoName'].unique():
-    state_data.append(file['GeoName'] == state)
-    
+#read the data from the excel file
+df = pd.read_excel('C:\Users\kmanske343\Documents\GitHub\GDP-in-relation-to-Industries\SAGDP2N__ALL_AREAS_1997_2022_cleaned.xlsx')
 
-
+#plot the data
+df.plot(x='Year', y='GDP', kind='line')
+plt.show()
