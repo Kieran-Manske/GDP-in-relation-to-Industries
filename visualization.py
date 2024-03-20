76a8cfb38b143870dad74d80278455cb8c6a2cd3
Kieran Mansk <kmanske343@lsr7.net>
2024-03-20 07:56:48 -0500

    pick state function

--- visualization.py
+++ visualization.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#read the data from the excel file
-df = pd.read_excel('C:\Users\kmanske343\Documents\GitHub\GDP-in-relation-to-Industries\SAGDP2N__ALL_AREAS_1997_2022_cleaned.xlsx')
 
-#plot the data
-df.plot(x='Year', y='GDP', kind='line')
-plt.show()
+df = pd.read_excel("C:/Users/kmanske343/Documents/GitHub/GDP-in-relation-to-Industries/SAGDP2N__ALL_AREAS_1997_2022_cleaned.xlsx")
+
+
+industries = df['Description'].unique()
+print(len(industries))
+
+def state_of_choice(df, State):
+    
+    state_df = df.loc[df['GeoName'] == State]
+    state_df = state_df.set_index('Description')
+    print(state_df)
+
+state_of_choice(df, 'Alabama')
+
+
