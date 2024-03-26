855700c40c46856554a831671d56a7ac29d38252
kmanske343 <kmanske343@lsr7sd.org>
2024-03-26 10:00:15 -0500

    industries?

--- visualization.py
+++ visualization.py
@@ -33,6 +33,23 @@ for state in df['GeoName'].unique():
     print(f'{state} has been saved to a csv file')
 
 
+#find the industry total for each unique geoname from 1997-2021
+
+Industry_totals = df.loc[df['Description'] == 'All industry total']
+Industry_totals = Industry_totals.set_index('GeoName')
+Industry_totals = Industry_totals.drop(['Description'], axis=1)
+Industry_totals = Industry_totals.transpose()
+
+#join years_df with Industry_totals
+
+Industry_totals = Industry_totals.join(years_df)
+#turn into a csv file
+#include years in the csv file
+Industry_totals.to_csv('Industry_totals.csv')
+
+    
+
+
 
 
 #turn this dataframe into a text file csv
